33aae1bbf6f71943ce0d675d74d13167f70cd47b
HannahFreitas <hannahcassia@gmail.com>
2022-03-30 19:15:32 -0300

    Inicializando o server

---
+++ universitario/universitario/src/main/java/com/sistema/universitario/UniversitarioApplication.java
@@ -0,0 +1,13 @@
+package com.sistema.universitario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversitarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversitarioApplication.class, args);
+	}
+
+}
