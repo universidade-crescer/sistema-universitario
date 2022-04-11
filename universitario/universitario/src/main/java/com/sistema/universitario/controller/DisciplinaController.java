4a189de2e44eacd09fd575c7c6f5f41e7f0761a2
Rejane-tech <rejaneims@hotmail.com>
2022-04-11 10:56:48 -0300

    Criando, atualizando e deletando a disciplina

--- universitario/universitario/src/main/java/com/sistema/universitario/controller/DisciplinaController.java
+++ universitario/universitario/src/main/java/com/sistema/universitario/controller/DisciplinaController.java
@@ -2,9 +2,8 @@ package com.sistema.universitario.controller;
 
 import com.sistema.universitario.models.Disciplina;
 import com.sistema.universitario.repositories.DisciplinaRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,6 +20,29 @@ public class DisciplinaController {
     @GetMapping
     public List<Disciplina> getAllDiscplina() {
         return this.disciplinaRepository.findAll();
+    }
+
+    @PostMapping
+    public Disciplina save(@RequestBody Disciplina disciplina) {
+        return disciplinaRepository.save(disciplina);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Disciplina> update(@PathVariable Long id, @RequestBody Disciplina disciplina) {
+        return disciplinaRepository.findById(id)
+                .map(save -> { save.setNome(disciplina.getNome());
+                                Disciplina newSave = disciplinaRepository.save(save);
+                                return ResponseEntity.ok().body(newSave);})
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        return disciplinaRepository.findById(id)
+                .map(delete -> {
+                    disciplinaRepository.deleteById(id);
+                    return ResponseEntity.ok().build();
+                }).orElse(ResponseEntity.notFound().build());
     }
 
 }
