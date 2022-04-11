9a688e62b1c16df319710b2b5e19b04296688cec
enzocosta99 <enzo99costa@gmail.com>
2022-04-11 11:53:16 -0300

    Delete para turma e um update para aluno

--- universitario/universitario/src/main/java/com/sistema/universitario/controller/AlunoController.java
+++ universitario/universitario/src/main/java/com/sistema/universitario/controller/AlunoController.java
@@ -1,7 +1,9 @@
 package com.sistema.universitario.controller;
 
 import com.sistema.universitario.models.Aluno;
+import com.sistema.universitario.models.Disciplina;
 import com.sistema.universitario.repositories.AlunoRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,6 +25,20 @@ public class AlunoController {
     @PostMapping
     public Aluno save(@RequestBody Aluno aluno){
         return this.alunoRepository.save(aluno);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Aluno> update(@PathVariable Long id, @RequestBody Aluno aluno) {
+        return alunoRepository.findById(id)
+                .map(save -> { save.setNome(aluno.getNome());
+                                save.setRua(aluno.getRua());
+                                save.setBairro(aluno.getBairro());
+                                save.setCidade(aluno.getCidade());
+                                save.setNum(aluno.getNum());
+                                save.setCep(aluno.getCep());
+                    Aluno newSave = alunoRepository.save(save);
+                    return ResponseEntity.ok().body(newSave);})
+                .orElse(ResponseEntity.notFound().build());
     }
 
 
--- universitario/universitario/src/main/java/com/sistema/universitario/controller/TurmaController.java
+++ universitario/universitario/src/main/java/com/sistema/universitario/controller/TurmaController.java
@@ -1,7 +1,9 @@
 package com.sistema.universitario.controller;
 
+import com.sistema.universitario.models.Disciplina;
 import com.sistema.universitario.models.Turma;
 import com.sistema.universitario.repositories.TurmaRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,5 +23,15 @@ public class TurmaController {
     @PostMapping
     public Turma save(@RequestBody Turma turma) {
         return turmaRepository.save(turma);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        return turmaRepository.findById(id)
+                .map(delete -> {
+                    turmaRepository.deleteById(id);
+                    return ResponseEntity.ok().build();
+                }).orElse(ResponseEntity.notFound().build());
     }
 }
